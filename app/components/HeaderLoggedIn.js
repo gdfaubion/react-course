fe6417ee4129e3020d987076c85a9da4ed781fc1
Grace Faubion <grace@monkee-boy.com>
2023-02-07 15:06:37 -0600

    start using Reducer and Immer

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useContext } from "react"
 import { Link } from "react-router-dom"
-import ExampleContext from "../ExampleContext"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 function HeaderLoggedIn(props) {
-  const { setLoggedIn } = useContext(ExampleContext)
+  const appDispatch = useContext(DispatchContext)
+  const appState = useContext(StateContext)
 
   function handleLoggedOut() {
-    setLoggedIn(false)
-    localStorage.removeItem("complexappToken")
-    localStorage.removeItem("complexappUsername")
-    localStorage.removeItem("complexappAvatar")
+    appDispatch({ type: "logout" })
   }
 
   return (
@@ -22,7 +21,7 @@ function HeaderLoggedIn(props) {
         <span className="chat-count-badge text-white"> </span>
       </span>
       <a href="#" className="mr-2">
-        <img className="small-header-avatar" src={localStorage.getItem("complexappAvatar")} />
+        <img className="small-header-avatar" src={appState.user.avatar} />
       </a>
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
